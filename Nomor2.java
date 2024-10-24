87c33f142027db8637ca02b4a88e62b5c6cda8d3
VikryAchmadSonjayaKusumah <vikryachmadsoenjayakusumah@gmail.com>
2024-10-24 12:44:29 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,29 @@
 public class Nomor2{
     public static void main(String[] args){
+import java.util.Scanner;
 
+public class nomor2{
+    public static void main(String[] args){
+        String beliau, mereka, kita;
+        String nama;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(s:"Masukan nama driver" );
+        beliau = scanner.next();
+        
+        System.out.println(s:"Masukan nama driver" );
+        mereka = scanner.next();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("lama trip: ");
+        4 = scanner.next();
+        
+        System.out.print("lama trip: ");
+        2 = scanner.next();
+
+        System.out.print("lama trip: ");
+        5 = scanner.nextInt();
+            
     }
 }
